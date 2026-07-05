40ee62dd271bb20b9712001f1119335b0b3b34ef
chrisostomemataba <doctorchrisostome@gmail.com>
2026-07-05 18:51:45 +0300

    refactor layout structure and organize imports in default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,9 @@
-<!-- layouts/default.vue -->
 <template>
   <div class="min-h-screen flex flex-col">
     <PaywallOverlay />
     <AppHeader />
-
     <div class="flex flex-1 pt-16">
       <CustomSidebar />
-
       <main
         :class="[
           'flex-1 p-6 transition-all duration-200',
@@ -17,19 +14,24 @@
       </main>
       <Toaster />
     </div>
-
     <AppFooter />
   </div>
 </template>
 
 <script setup>
-// Get shared sidebar state
-const sidebarCollapsed = useState('sidebar-collapsed', () => false)
-
 import 'vue-sonner/style.css'
 import { Toaster } from '@/components/ui/sonner'
 import PaywallOverlay from '@/components/license/PaywallOverlay.vue'
-definePageMeta({
-  middleware: 'permissions'
+import { useAuth } from '~/composables/useAuth'
+import { usePermissions } from '~/composables/usePermissions'
+
+const sidebarCollapsed = useState('sidebar-collapsed', () => false)
+const { user } = useAuth()
+const { fetchUserPermissions } = usePermissions()
+
+onMounted(async () => {
+  if (user.value?.id) {
+    await fetchUserPermissions(user.value.id)
+  }
 })
 </script>
